55cffb72a01d433db8d7d2e536d0f619aec02cbb
hossainks <shakirhossain.qa@gmail.com>
2025-10-05 22:29:57 +0600

    Run test paralelly

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,31 +1,14 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  workers: process.env.CI ? undefined : undefined,
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:4200',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
 
@@ -36,15 +19,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-/*     {
+    {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    }, */
+    /*
+        {
+          name: 'webkit',
+          use: { ...devices['Desktop Safari'] },
+        }, */
   ],
 
   /* Run your local dev server before starting the tests */
