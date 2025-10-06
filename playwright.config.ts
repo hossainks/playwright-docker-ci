9c1e64c48a8303b297552573855639c34e823514
hossainks <shakirhossain.qa@gmail.com>
2025-10-06 10:55:13 +0600

    Increase workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? undefined : undefined,
+  workers: process.env.CI ? 5 : undefined,
   reporter: [
     ['html', { open: 'never' }],
     ['junit', { outputFile: 'test-results/test-results.xml' }],
